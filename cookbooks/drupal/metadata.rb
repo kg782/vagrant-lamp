21387d0715f74a0b7010c9198476c48fcd3a8824
kg782 <keigomuto@gmail.com>
2016-09-22 14:43:33 +1000

    added drupal cookbook

---
+++ cookbooks/drupal/metadata.rb
@@ -0,0 +1,7 @@
+name 'drupal'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures drupal'
+long_description 'Installs/Configures drupal'
+version '0.1.0'
---
+++ cookbooks/drupal/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: drupal
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'ruby-compass'
