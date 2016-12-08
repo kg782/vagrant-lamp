da7015298bb90ccef9bd433cfed8598107a5e518
kg782 <keigomuto@gmail.com>
2016-12-08 14:25:02 +1100

    added drush install script

--- cookbooks/drupal/recipes/default.rb
+++ cookbooks/drupal/recipes/default.rb
@@ -5,3 +5,17 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 package 'ruby-compass'
+
+bash "drush" do
+  user "root"
+  cwd "/home/vagrant"
+  code <<-EOF
+    if ! type drush > /dev/null; then
+      php -r "readfile('https://s3.amazonaws.com/files.drush.org/drush.phar');" > drush
+      php drush core-status
+      chmod +x drush
+      mv drush /usr/local/bin
+      drush init --no-add-path
+    fi
+  EOF
+end
